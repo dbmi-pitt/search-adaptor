d8c1a5d1c7fd9437279dba2db035bfc4fd8bf29b
Karl Burke <kburke@pitt.edu>
2022-11-01 15:32:34 -0400

    Change endpoint route variable order for compatibility with AWS Gateway requirements.

--- src/app.py
+++ src/app.py
@@ -106,7 +106,7 @@ class SearchAPI:
             return self.add(uuid, index)
 
         @self.app.route('/clear-docs/<index>', methods=['POST'])
-        @self.app.route('/clear-docs/<uuid>/<index>', methods=['POST'])
+        @self.app.route('/clear-docs/<index>/<uuid>', methods=['POST'])
         def __clear_docs(index, uuid=None):
             return self.clear_docs(index, uuid)
 
