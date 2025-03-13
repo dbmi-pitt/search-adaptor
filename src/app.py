499366b89da102c278bf32c31c66085a228127ff
Tyler Madonna <tjm159@pitt.edu>
2025-03-13 16:51:55 -0400

    Fixing SearchAPI constructor for positional args

--- src/app.py
+++ src/app.py
@@ -29,7 +29,7 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s:%(lineno)d
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 class SearchAPI:
-    def __init__(self, config, translator_module, progress_interface=None, blueprint=None, ubkg_instance=None):
+    def __init__(self, config, translator_module, blueprint=None, ubkg_instance=None, progress_interface=None):
         # Set self based on passed in config parameters
         for key, value in config.items():
             setattr(self, key, value)
